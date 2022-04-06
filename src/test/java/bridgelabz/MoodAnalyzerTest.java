c7d6e842965b3a887e54e14183dfe19228e4622c
pavithramuddu93 <pavithramuddu93>
2022-04-06 09:20:39 +0530

    UC3 added

--- src/test/java/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/bridgelabz/MoodAnalyzerTest.java
@@ -4,33 +4,58 @@ import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertEquals;
 public class MoodAnalyzerTest {
-        MoodAnalyzer moodAnalyzer;
 
-        @Test
-        public void givenSad_shouldReturnSad() {
-            moodAnalyzer = new MoodAnalyzer("SAD");
-            String result = moodAnalyzer.analyseMood();
-            assertEquals("SAD", result);
+    MoodAnalyzer moodAnalyzer;
 
-        }
+    @Test
+    public void givenSad_shouldReturnSad() throws MoodAnalyzerException {
+        moodAnalyzer = new MoodAnalyzer("SAD");
+        String result = moodAnalyzer.analyseMood();
+        assertEquals("SAD", result);
 
-        @Test
-        public void givenAny_shouldReturnHappy() {
-            moodAnalyzer = new MoodAnalyzer("any");
-            String result = moodAnalyzer.analyseMood();
-            assertEquals("HAPPY", result);
+    }
 
-        }
+    @Test
+    public void givenAny_shouldReturnHappy() throws MoodAnalyzerException {
+        moodAnalyzer = new MoodAnalyzer("any");
+        String result = moodAnalyzer.analyseMood();
+        assertEquals("HAPPY", result);
 
-        @Test
-        public void givenNull_shouldReturnHappy() {
-            MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
-            String result = null;
-            try {
-                result = moodAnalyser.analyseMood();
-                Assert.assertEquals("HAPPY", result);
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-            }
+    }
+
+    @Test
+    public void givenNull_shouldReturnHappy() throws MoodAnalyzerException {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
+        String result = null;
+        try {
+            result =  moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", result);
+        }catch (NullPointerException e){
+            e.printStackTrace();
         }
     }
+
+    @Test
+    public void givenNull_shouldThrowExceptionAsNullInput(){
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
+        String result = null;
+        try {
+            result =  moodAnalyser.analyseMood();
+            Assert.assertEquals(null, result);
+        }catch (NullPointerException | MoodAnalyzerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenEmpty_shouldThrowExceptionAsEmptyInput(){
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("");
+        String result = null;
+        try {
+            result =  moodAnalyser.analyseMood();
+            Assert.assertEquals(ExceptionMessage.Empty, result);
+        }catch (NullPointerException | MoodAnalyzerException e){
+            e.printStackTrace();
+        }
+    }
+}
